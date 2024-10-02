a2ac4e7552f0a2899023c275ac6fdc8d5fefd9f1
Godwin Bruce <godwin.bruce@gmail.com>
2024-10-02 15:24:22 -0700

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,28 +3,17 @@
 Main file for testing
 """
 
+
 def makeChange(coins, total):
-    """
-    How many of this type of coin can I get with my money? Okay,
-        I'll take that many. Now, how much money do I have left?
-        And how many coins do I have in my pocket?
-    """
-    if total <= 0:
+    """ making changes  """
+    if total < 1:
         return 0
-    elif total > 0:
-        newList = sorted(coins[:])
-        newList = list(reversed(newList))
-        count = 0
-        value = total + 0
-        index = 0
-        while value >= 0 and (index < len(newList)):
-            if value >= newList[index]:
-                value = value - newList[index]
-                count += 1
-            elif value < newList[index]:
-                index += 1
-        if index == len(newList):
-            if value != 0:
-                return -1
-            elif value == 0:
-                return count
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        if total == 0:
+            break
+        num = total // coin
+        total -= num * coin
+        count += num
+    return count if total == 0 else -1
